7e7898c05b8f52955bb33d81739ffc33cef22a02
Tomasz <tomigunpl@icloud.com>
2022-05-17 20:14:10 +0200

    Finished project and whole course

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '8d7dc019-7f56-4665-a3f5-230c3d394255';
+export const MODAL_CLOSE_SEC = 2.5;
